0d4c981cd08a7cb265dcc9fd52493001989d5c43
PammyLo <pamela@tuminds.com>
2019-09-21 10:57:05 +0100

    revistited structure, replaced film_id with screening_id in ticket and removed ticket_id from screening.

--- models/screening.rb
+++ models/screening.rb
@@ -6,9 +6,9 @@ class Screening
   def initialize(details)
     @id = details['id'].to_i if details['id']
     @start_time = 'start_time'.to_datetime
-    @ticket_id = 'ticket_id'
     @film_id = 'film_id'
-  end 
+  end
+  
 
 
 end
--- models/ticket.rb
+++ models/ticket.rb
@@ -5,8 +5,10 @@ class Ticket
 
   def initialize(details)
     @id = details['id'].to_i if details['id']
-    @customer_id = 'customer_id'.to_i
-    @film_id = 'film_id'.to_i
+    @customer_id = details['customer_id'].to_i
+    @screening_id = ['screening_id'].to_i
   end
+
+
 
 end
