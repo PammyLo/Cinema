15b70482a6a96c4bccfc859ffbfa299e1adbf67d
PammyLo <pamela@tuminds.com>
2019-09-21 09:45:24 +0100

    customer CRUD completed

--- console.rb
+++ console.rb
@@ -5,6 +5,9 @@ require_relative('./models/screening.rb')
 
 require 'pry'
 
+
+Customer.delete_all
+
 customer1 = Customer.new({'name' => 'Jo Blog', 'funds' => '50'})
 customer2 = Customer.new({'name' => 'Blair Jones', 'funds' => '80'})
 customer3 = Customer.new({'name' => 'Karen Smith', 'funds' => '60'})
@@ -12,5 +15,12 @@ customer3 = Customer.new({'name' => 'Karen Smith', 'funds' => '60'})
 customer1.save()
 customer2.save()
 customer3.save()
+
+customer2.delete
+
+customer1.funds = 75
+customer1.update
+
+p customer3.customer_by_id
 
 binding.pry
--- models/customer.rb
+++ models/customer.rb
@@ -6,15 +6,44 @@ attr_accessor :name, :funds
 
   def initialize(details)
     @id = details['id'].to_i if details['id']
-    @name = 'name'
-    @funds = 'funds'.to_i
+    @name = details['name']
+    @funds = details['funds'].to_i
   end
 
   def save
     sql = 'INSERT INTO customers ( name, funds )
           VALUES ($1, $2) RETURNING id'
     values = [ @name, @funds ]
+    customer = SqlRunner.run( sql, values )[0]
+    @id = customer['id'].to_i
+  end
+
+  def Customer.delete_all
+    sql = 'DELETE FROM customers'
+    SqlRunner.run( sql )
+  end
+
+  def delete
+    sql = 'DELETE FROM customers
+          WHERE @id = $1'
+    values = [ @id ]
     SqlRunner.run( sql, values )
+  end
+
+  def update
+    sql = "UPDATE customers
+          SET name = $1, funds = $2
+          WHERE id = $3"
+    values = [ @name, @funds, @id ]
+    SqlRunner.run( sql, values)
+  end
+
+  def customer_by_id
+    sql = 'SELECT name FROM customers
+          WHERE id = $1'
+    values = [ @id ]
+    customer = SqlRunner.run( sql, values ).first
+    return customer
   end
 
 
